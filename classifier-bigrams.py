7d90c0be70ebd7a31d0e48f2e43c861b74fb543a
asketak <simatomas@seznam.cz>
2016-04-24 16:03:25 +0200

    refactoring classifier code

--- classifier-bigrams.py
+++ classifier-bigrams.py
@@ -5,25 +5,27 @@ import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
  
-def bigram_word_feats(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
-    bigram_finder = BigramCollocationFinder.from_words(words)
-    bigrams = bigram_finder.nbest(score_fn, n)
-    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
- 
+train_test_ratio = 2.0/3
 
-negids = movie_reviews.fileids('neg')
-posids = movie_reviews.fileids('pos')
+def bigram_words_in_sentence(words, score_fn=BigramAssocMeasures.dice, n=200):
+	bigram_finder = BigramCollocationFinder.from_words(words)
+	bigrams = bigram_finder.nbest(score_fn, n)
+	return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
  
-negfeats = [(bigram_word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
-posfeats = [(bigram_word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
  
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
+files_in_neg = movie_reviews.fileids('neg')
+files_in_pos = movie_reviews.fileids('pos')
  
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+neg_data = [(bigram_words_in_sentence(movie_reviews.words(fileids=[f])), 'neg') for f in files_in_neg]
+pos_data = [(bigram_words_in_sentence(movie_reviews.words(fileids=[f])), 'pos') for f in files_in_pos]
  
-classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+negative_first_test_pos = int(len(neg_data)*train_test_ratio)
+positive_first_test_pos = int(len(pos_data)*train_test_ratio)
+ 
+train_data = neg_data[:negative_first_test_pos] + pos_data[:positive_first_test_pos]
+test_data = neg_data[negative_first_test_pos:] + pos_data[positive_first_test_pos:]
+print 'training on %d paragraphs and testing on %d paragraphs' % (len(train_data), len(test_data))
+ 
+classifier = NaiveBayesClassifier.train(train_data)
+print 'accuracy:', nltk.classify.util.accuracy(classifier, test_data)
 classifier.show_most_informative_features(20)
--- classifier.py
+++ classifier.py
@@ -2,22 +2,23 @@ import nltk.classify.util
 from nltk.classify import NaiveBayesClassifier
 from nltk.corpus import movie_reviews
  
-def word_feats(words):
+train_test_ratio = 2.0/3
+def words_in_sentence(words):
     return dict([(word, True) for word in words])
  
-negids = movie_reviews.fileids('neg')
-posids = movie_reviews.fileids('pos')
+files_in_neg = movie_reviews.fileids('neg')
+files_in_pos = movie_reviews.fileids('pos')
  
-negfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in negids]
-posfeats = [(word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in posids]
+neg_data = [(words_in_sentence(movie_reviews.words(fileids=[f])), 'neg') for f in files_in_neg]
+pos_data = [(words_in_sentence(movie_reviews.words(fileids=[f])), 'pos') for f in files_in_pos]
  
-negcutoff = len(negfeats)*3/4
-poscutoff = len(posfeats)*3/4
+negative_first_test_pos = int(len(neg_data)*train_test_ratio)
+positive_first_test_pos = int(len(pos_data)*train_test_ratio)
  
-trainfeats = negfeats[:negcutoff] + posfeats[:poscutoff]
-testfeats = negfeats[negcutoff:] + posfeats[poscutoff:]
-print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+train_data = neg_data[:negative_first_test_pos] + pos_data[:positive_first_test_pos]
+test_data = neg_data[negative_first_test_pos:] + pos_data[positive_first_test_pos:]
+print 'training on %d paragraphs and testing on %d paragraphs' % (len(train_data), len(test_data))
  
-classifier = NaiveBayesClassifier.train(trainfeats)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
-classifier.show_most_informative_features()
+classifier = NaiveBayesClassifier.train(train_data)
+print 'accuracy:', nltk.classify.util.accuracy(classifier, test_data)
+classifier.show_most_informative_features(20)
